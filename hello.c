f1ffc0de606fb41b2b8ea11efe1180332b63a9b2
nullwill <williamaldag03@gmail.com>
2023-08-25 16:25:24 -0500

    checking

--- hello.c
+++ hello.c
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 
     printf("Jake Vaughn!\n");
     printf("My major is Data Science!\n");
-    printf("William Aldag\n");
-    printf("My major is Computer Science\n");
+    printf("William Aldag!\n");
+    printf("My major is Computer Science!\n");
 
 }
